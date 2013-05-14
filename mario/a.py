c2ab79b936bd9f4c34606613f4a7f7ee9ef9b96c
mateusz <mateusz@mateusz-G33-DS3R>
2013-05-14 22:08:36 +0200

    Przed informatyką
    Dodaję jakąś podstawową animację i model, żeby było co ogarniać na
    informatyce.

--- mario/a.py
+++ mario/a.py
@@ -1,1 +1,27 @@
-print 'Hello world!2'
+from direct.showbase.ShowBase import ShowBase
+from direct.actor.Actor import Actor
+
+class MyApp(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+        #self.pa = loader.loadModel("/home/mateusz/Pulpit/PANDA/working.egg")
+        #self.pa = loader.loadModel("/home/mateusz/Pulpit/untitled.x")
+        #self.pa.setScale(1000, 1000, 1000)
+        self.pa = Actor("./animation.x", {"foobar": "./animation.x",})
+        self.pa.setPos(0, 5, 0)
+        self.pa.reparentTo(self.render)
+        #self.pa.show()
+        self.pa.loop("foobar")
+        print self.pa.getPos()
+        self.accept("a", self.keyFunction)
+        
+    def keyFunction(self):
+        print "a"
+        self.pa.stop()
+        print self.pa.node()
+        self.pa.lookAt(5, 5, 0)
+        self.pa.loop("foobar")
+        
+        
+app = MyApp()
+app.run()
